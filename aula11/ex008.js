8217a97e0c71f1304253c1e4a0dcf0c08f515a22
Higor Romano <higorjromano@gmail.com>
2025-12-01 15:07:49 -0300

    criei ex011

---
+++ aula11/ex008.js
@@ -0,0 +1,6 @@
+var vel = 60.5 
+console.log(`A velocidade do seu carro é ${vel}Km/h`)
+if (vel > 60) {
+    console.log('Você ultrapassou o limite de velocidade!')
+}
+console.log('Dirija com cinto de segurança!')
